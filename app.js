967c91d37c10733467765ded9e5e015f9491d895
ajalil01 <ahmed.benzid@etu-univ-batna2.dz>
2025-04-10 08:02:24 -0700

    Added email sending logic of get in touch with nodemailer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express= require("express");
 const authRoutes = require('./backend/routes/auth-routes');
+const mailRoutes = require('./backend/routes/mail-routes');
 const consectDB=require("./backend/database/db.js");
 
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/users', authRoutes);
+app.use('/api/mail',mailRoutes)
 
 const PORT= process.env.PORT||3000;
 

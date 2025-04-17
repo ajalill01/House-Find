f8c05bcc6fe61d81bc5cb5452912d545f5ea8fde
ajalil01 <ahmed.benzid@etu-univ-batna2.dz>
2025-04-17 12:00:01 -0700

    jalil add upload post and cloudinary things

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express= require("express");
 const authRoutes = require('./backend/routes/auth-routes');
 const mailRoutes = require('./backend/routes/mail-routes');
 const userRoutes = require('./backend/routes/user-routes.js')
+const uploadPostRoutes = require('./backend/routes/post-routes.js')
+
 const consectDB=require("./backend/database/db.js");
 
 
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use('/api/users',authRoutes);
 app.use('/api/mail',mailRoutes)
 app.use('/api/users',userRoutes)
+app.use('/api/post',uploadPostRoutes)
 
 
 const PORT= process.env.PORT||3000;
---
+++ backend/controllers/post-controllers.js
@@ -0,0 +1,76 @@
+const Post = require('../model/Post')
+const {uploadToCloudinary} = require('../helpers/cloudinary-helpers')
+const fs = require('fs')
+
+const uploadPost = async(req,res)=>{
+    try{
+        const postedBy = req.userInfo.userId
+
+        const {
+            title,
+            description,
+            email,
+            phoneNumber,
+            city,
+            googleMapLink,
+            type,
+            propertyType,
+            price,
+        } = req.body
+
+        const files = req.files
+        if (!files || files.length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: "No images uploaded" 
+            });
+          }
+
+       const filePaths = files.map(file => file.path);
+
+       const uploadesImages = await uploadToCloudinary(filePaths)
+
+       const newPost = new Post({
+        title,
+        description,
+        email,
+        phoneNumber,
+        city,
+        googleMapLink,
+        type,
+        propertyType,
+        price,
+        photos: uploadesImages,
+        postedBy
+       })
+
+       await newPost.save()
+
+       //Delete files from uploads/
+       await Promise.all(
+        filePaths.map((filePath) =>
+          fs.promises.unlink(filePath).catch((error) => {
+            console.error(`Failed to delete file ${filePath}: `, error);
+          })
+        )
+      );
+
+       res.status(201).json({
+        success : true,
+        message : 'Post uploaded successfully',
+        post : newPost
+       })
+
+    }
+    catch(e){
+        console.log('Error from  uploadPost\n',e)
+        res.status(500).json({
+            success : false,
+            message : 'Error while uploading post', 
+        })
+    }
+}
+
+module.exports = {
+    uploadPost
+}

02e2eddf1d5e1d7e81c0835ac974d328d09f98d8
ajalil01 <ahmed.benzid@etu-univ-batna2.dz>
2025-04-18 06:48:53 -0700

    jalil add pagniation

--- backend/controllers/stats-controllers.js
+++ backend/controllers/stats-controllers.js
@@ -1,21 +1,30 @@
-const User = require('../model/User')
+const User = require('../model/User');
 
-const getAllUsers = async (req,res)=>{
-    try{
-        const users = await User.find({ role: 'user' }).lean();
+const getAllUsers = async (req, res) => {
+    try {
+
+        const page = parseInt(req.query.page) || 1;
+        const limits = parseInt(req.query.limits) || 10;
+        const skip = (page-1)*limits;
+
+        const users = await User.find({ role: 'user' }).skip(skip).limit(limits).lean();
+        const totalUsers = await User.countDocuments({ role: 'user' });
+
         res.status(202).json({
-            success : true,
-            message : 'Get all users is done',
-            users : users
-        })
-    }
-    catch(e){
-        console.log('Error from getAllUsers\n',e)
+            success: true,
+            message: 'Get all users is done',
+            currentPage: page,
+            totalPages: Math.ceil(totalUsers / limits),
+            totalUsers: totalUsers,
+            users: users
+        });
+    } catch (e) {
+        console.log('Error from getAllUsers\n', e);
         res.status(500).json({
-            success : false,
-            message : 'Error from get all users'
-        })
+            success: false,
+            message: 'Error from get all users'
+        });
     }
-}
+};
 
-module.exports = {getAllUsers}
+module.exports = { getAllUsers };
